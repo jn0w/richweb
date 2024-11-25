005cd3c6b1ee3cb109dd9e8bd9dfe07beff58194
jn0w <B00153676@mytudublin.ie>
2024-11-25 19:31:50 +0000

    react18

---
+++ react18/ironsessions/assig1/my-app/src/app/api/getWeather/route.js
@@ -0,0 +1,28 @@
+import { NextResponse } from "next/server";
+
+export async function GET(req) {
+  try {
+    console.log("Fetching weather data...");
+
+    // Your API key and location
+    const apiKey = "527ae76060d44929a96181457241911"; // Your Weather API key
+    const location = "Dublin"; // You can replace this with a dynamic or preferred location
+    const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}&aqi=no`;
+
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("Failed to fetch weather data");
+    }
+
+    const data = await response.json();
+    const currentTemp = data.current.temp_c;
+
+    return NextResponse.json({ temp: currentTemp });
+  } catch (error) {
+    console.error("Error fetching weather data:", error);
+    return NextResponse.json(
+      { error: "Failed to fetch weather data" },
+      { status: 500 }
+    );
+  }
+}
